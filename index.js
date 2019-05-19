4e15802245755ed0570841b1d8ad527d32e5f99b
Anil Kishan <kishan.anil@gmail.com>
2019-05-19 01:22:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var number;
-function add(a, b){ //add a and b and return the result.
+function add(a, b){
   number = a + b;
   return number;
 }
 
-function subtract(a,b){
-  return `a-b`
+function substract(a, b){
+  number = a + b;
+  return number;
 }
 
-function multiply(a,b){
-  return `a*b`
+
+function add(a, b){
+  number = a + b;
+  return number;
 }
 
-function divide(a,b){
-  return `a / b`
+function add(a, b){
+  number = a + b;
+  return number;
 }
